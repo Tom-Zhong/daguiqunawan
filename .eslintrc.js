0fd41d571607b68e57c818a8bd5ca91660bdb885
ZhongGuiyong <814374557@qq.com>
2018-11-05 07:47:20 +0800

    welecom page done

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,9 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    // "no-extra-semi": 2,
+    // "semi": ["error", "never"]
+    
   }
 }
